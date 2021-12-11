11ba9556e8058a764859535c1ca352ba57a73cf9
Shiran <shiranDapht1@gmail.com>
2021-12-11 20:27:12 +0200

    more

--- node_set.c
+++ node_set.c
@@ -10,3 +10,33 @@ struct AmountSet_t{
     Node head;
     Node current;
 };
+
+char* copyItemName(const char* itemName){
+    if(!itemName){
+        
+    }
+}
+
+int compareItemNames(const char* itemName1, const char* itemName2);
+
+void freeItemName(char* itemName);
+
+char* getItemName(Node node);
+
+void setItemName(Node node, char* itemName);
+
+double getAmount(Node node);
+
+void setAmount(Node node, double amount);
+
+Node getNext(Node node);
+
+void setNext(Node node ,Node next);
+
+Node getHead(AmountSet as);
+
+void setHead(AmountSet as, Node node);
+
+Node getCurrent(AmountSet as);
+
+void setCurrent(AmountSet as, Node current);

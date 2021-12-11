319e17a4db71649f2b404c772fb3e34368d3f3ce
Shiran <shiranDapht1@gmail.com>
2021-12-11 20:54:29 +0200

    must replace "mid finger"

--- node_set.c
+++ node_set.c
@@ -1,9 +1,10 @@
 #include "node_set.h"
 
+
 struct node_t{
-    char* itemName;
-    double amount;
-    Node next; 
+    char* itemName_t;
+    double amount_t;
+    Node next_t; 
 };
 
 struct AmountSet_t{
@@ -13,21 +14,71 @@ struct AmountSet_t{
 
 char* copyItemName(const char* itemName){
     if(!itemName){
-        
+        return NULL;
     }
+    char* itemNameCopy = (char*)malloc(strlen(itemName)+1);
+    if(!itemNameCopy){
+        return NULL;
+    }
+    strcpy(itemNameCopy, itemName);
+    return itemNameCopy;
 }
 
-int compareItemNames(const char* itemName1, const char* itemName2);
 
-void freeItemName(char* itemName);
+/**
+ * @brief make sure to check null argument before use
+ * 
+ * @param itemName1 
+ * @param itemName2 
+ * @return int 
+ */
+int compareItemNames(const char* itemName1, const char* itemName2){
+    return strcmp(itemName1, itemName2);
+}
 
-char* getItemName(Node node);
+void freeItemName(char* itemName){
+    if(!itemName){
+        return;
+    }
+    free(itemName);
+}
 
-void setItemName(Node node, char* itemName);
+Node createNode(char* itemName, double amount){
 
-double getAmount(Node node);
+}
 
-void setAmount(Node node, double amount);
+void deleteNode(Node node){
+    
+}
+
+char* getItemName(Node node){
+    if(!node){
+        return NULL;
+    }
+    return node->itemName_t;
+}
+
+void setItemName(Node node, char* itemName){
+    if(!node || !itemName){
+        return;
+    }
+    freeItemName(node->itemName_t);
+    node->itemName_t = copyItemName(itemName);
+}
+
+double getAmount(Node node){
+    if(!node){
+        return ERROR_AMOUNT;
+    }
+    return node->amount_t;
+}
+
+void setAmount(Node node, double amount){
+    if(!node){
+        return;
+    }
+
+}
 
 Node getNext(Node node);
 

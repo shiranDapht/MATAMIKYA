d150019083fef9dc5f1eff5fb2eaf71783eed033
Adam Katav <adamkatav@gmail.com>
2021-12-12 23:19:13 +0200

    finished item_qty

--- item_qty.h
+++ item_qty.h
@@ -1,10 +1,13 @@
 #ifndef ITEM_QTY_H_
 #define ITEM_QTY_H_
-
+#include <stdbool.h>
 typedef struct ItemQty_t* ItemQty;
 
 unsigned int getId(ItemQty item);
 bool setId(ItemQty item, unsigned int id);
+
+double getAmount(ItemQty item);
+bool setAmount(ItemQty item, double amount);
 
 
 #endif

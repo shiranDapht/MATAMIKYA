6e43a8a6bc704f7ff66cdfcb284f6ba8a1f450ea
Shiran <shiranDapht1@gmail.com>
2021-12-14 04:33:56 +0200

    for shiran

--- matamikya.c
+++ matamikya.c
@@ -15,12 +15,12 @@ Matamikya matamikyaCreate(){
     if(!new_matamikya){
         return NULL;
     }
-    LinkedList waerhouse = (LinkedList)malloc(sizeof(LinkedList_t));
+    LinkedList waerhouse = (LinkedList)malloc(sizeof(waerhouse));
     if(!waerhouse){
         free(new_matamikya);
         return NULL;
     }
-    LinkedList orders = (LinkedList)malloc(sizeof(LinkedList_t));
+    LinkedList orders = (LinkedList)malloc(sizeof(orders));
     if(!orders){
         free(new_matamikya);
         return NULL;
@@ -39,6 +39,16 @@ void matamikyaDestroy(Matamikya matamikya){
     }
 }
 
+bool isNameValid(const char* name){
+    bool num = (name[0]>='0' && name[0]<='9'), A_Z = (name[0]>='A' && name[0]<='Z'),
+        a_z = (name[0]>='a' && name[0]<='z'); 
+    return (num || A_Z || a_z);
+}
+
+bool isAmountConsistene(const double amount, const MatamikyaAmountType amountType){
+
+}
+
 MatamikyaResult mtmNewProduct(Matamikya matamikya, const unsigned int id, const char *name,
                               const double amount, const MatamikyaAmountType amountType,
                               const MtmProductData customData, MtmCopyData copyData,
@@ -46,8 +56,25 @@ MatamikyaResult mtmNewProduct(Matamikya matamikya, const unsigned int id, const 
                                 
                                 if(!matamikya  || !name  || !amountType  || !customData  || !copyData  
                                         || !freeData  || !prodPrice){
-                                            
+                                       return MATAMIKYA_NULL_ARGUMENT;     
                                 }
+                                LL_FOREACH(unsigned int, it, matamikya->warehouse_t){
+                                    if(it == id){
+                                        return MATAMIKYA_PRODUCT_ALREADY_EXIST;
+                                    }
+                                }
+                                if(!isNameValid(name)){
+                                    return MATAMIKYA_INVALID_NAME;
+                                }
+                                //TODO: amunt type func by matamikya h file 
+                                if(!isAmountConsistene(amount,amountType)){
+                                    return MATAMIKYA_INVALID_AMOUNT;
+                                }
+
+                                if(!llAddNode(matamikya->warehouse_t, id, customData, freeData)){
+                                    return MATAMIKYA_OUT_OF_MEMORY;
+                                }
+                                return MATAMIKYA_SUCCESS;
                               }
 
 MatamikyaResult mtmChangeProductAmount(Matamikya matamikya, const unsigned int id, const double amount){

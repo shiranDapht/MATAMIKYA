92546b3e2b58585ec46d995981d76f0000bdc247
Shiran <shiranDapht1@gmail.com>
2021-12-14 02:29:17 +0200

    matamikay

--- matamikya.c
+++ matamikya.c
@@ -3,8 +3,10 @@
 #include "linked_list.h"
 #include "matamikya.h"
 
+#include <stdlib.h>
+
 struct Matamikya_t{
-    LinkedList stock_t;
+    LinkedList warehouse_t;
     LinkedList orders_t;
 };
 
@@ -13,5 +15,80 @@ Matamikya matamikyaCreate(){
     if(!new_matamikya){
         return NULL;
     }
-    LinkedList new_stock = (LinkedList)malloc()
+    LinkedList waerhouse = (LinkedList)malloc(sizeof(LinkedList_t));
+    if(!waerhouse){
+        free(new_matamikya);
+        return NULL;
+    }
+    LinkedList orders = (LinkedList)malloc(sizeof(LinkedList_t));
+    if(!orders){
+        free(new_matamikya);
+        return NULL;
+    }
+    return new_matamikya;
+}
+
+void matamikyaDestroy(Matamikya matamikya){
+    if(matamikya){
+        if(matamikya->warehouse_t){
+            deleteLinkedList(matamikya->warehouse_t);
+        }
+        if(matamikya->orders_t){
+            deleteLinkedList(matamikya->orders_t);
+        }
+    }
+}
+
+MatamikyaResult mtmNewProduct(Matamikya matamikya, const unsigned int id, const char *name,
+                              const double amount, const MatamikyaAmountType amountType,
+                              const MtmProductData customData, MtmCopyData copyData,
+                              MtmFreeData freeData, MtmGetProductPrice prodPrice){
+                                
+                                if(!matamikya  || !name  || !amountType  || !customData  || !copyData  
+                                        || !freeData  || !prodPrice){
+                                            
+                                }
+                              }
+
+MatamikyaResult mtmChangeProductAmount(Matamikya matamikya, const unsigned int id, const double amount){
+
+}
+
+MatamikyaResult mtmClearProduct(Matamikya matamikya, const unsigned int id){
+
+}
+
+unsigned int mtmCreateNewOrder(Matamikya matamikya){
+
+}
+
+MatamikyaResult mtmChangeProductAmountInOrder(Matamikya, const unsigned int orderId,
+                                     const unsigned int productId, const double amount){
+
+                                     }
+
+MatamikyaResult mtmShipOrder(Matamikya matamikya, const unsigned int orderId){
+
+}   
+
+MatamikyaResult mtmCancelOrder(Matamikya matamikya, const unsigned int orderId){
+
+}
+
+MatamikyaResult mtmPrintInventory(Matamikya matamikya, FILE *output){
+
+}
+
+MatamikyaResult mtmPrintOrder(Matamikya matamikya, const unsigned int orderId, FILE *output){
+
+}
+
+MatamikyaResult mtmPrintBestSelling(Matamikya matamikya, FILE *output){
+    if(!matamikya || !output){
+        return MATAMIKYA_NULL_ARGUMENT;
+    }
+
+    double best_selling = 0;
+
+    
 }
